736579b3bc04f6b6fc9c8c4cc55a45e43a859f40
Blake Runyon <blakerunyon09@gmail.com>
2021-02-04 15:15:12 -0700

    commit tree d3fbed4ecc941b18eafc493fca4d0feb08ff7d84

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -3,5 +3,9 @@ require_relative './student.rb'
 require_relative './environment.rb'
 require_relative './boatingtest.rb'
 
+bob = Student.new("Spongebob Squarepants")
+crab = Student.new("Mr. Krabs")
+starman = Student.new("Patrick Starfish")
+
 binding.pry
 0
